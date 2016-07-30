0fdbee5aa4f107a4516be97c3ab84ee562293068
Alex Ungurianu <iau2@st-andrews.ac.uk>
2016-07-30 12:23:09 +0100

    Add scafolding for classes

--- main.py
+++ main.py
@@ -6,17 +6,30 @@ class Parser:
 	def parse(self,exp_string):
 		pass
 
+class Evaluator:
+	def __init__(self):
+		pass
+	def eval(self,expression_ast):
+		return None
 
 def main():
+
 	arg_parser = argparse.ArgumentParser(description='Evaluate an mathematical expression.')
+	
+	# open is used as a type to retrieve a file from the filename given
 	arg_parser.add_argument('file', metavar='filename', type=open, 
-		help='File containing the expression to be evaulated.')
+		help='File containing the expression to be evaluated.')
 	
 	try:
 		args = arg_parser.parse_args()
 		expression_parser = Parser()
+		evaluator = Evaluator()
+
+		# read every expression from the given file
 		for line in args.file:
-			expression_parser.parse(line)
+			expression_ast = expression_parser.parse(line)
+			print(evaluator.eval(expression_ast))
+	
 	except FileNotFoundError:
 		print("Please pass a valid filename")
 
