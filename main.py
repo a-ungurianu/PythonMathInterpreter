80b338db1beca92096899a5d7919cf54de747664
Alex Ungurianu <iau2@st-andrews.ac.uk>
2016-07-30 16:57:14 +0100

    Implement number and factor parsers

--- main.py
+++ main.py
@@ -25,12 +25,33 @@ class Expression(ASTNode):
 		return self.operation(self.first_term, self.second_term)
 
 
+class Factor(ASTNode):
+	def __init__(self,child,negate=False):
+		self.negate = negate
+		self.child = child
+	
+	def eval(self):
+		if self.negate:
+			return -self.child.eval()
+		else:
+			return self.child.eval()
+
+class Number(ASTNode):
+	def __init__(self,number):
+		self.number = number
+	
+	def eval(self):
+		return self.number
+
 class Parser:
 
 	op_dict = { "+": lambda a, b: a + b,
 				"-": lambda a, b: a - b,
 				"*": lambda a, b: a * b,
 				"/": lambda a, b: a / b}
+
+	non_zero_digits = "123456789"
+	digits = "0" + non_zero_digits
 
 	def __init__(self):
 		self.stream = ""
@@ -83,18 +104,56 @@ class Parser:
 			# Look at whether next character in stream is a expr_op
 			expr_op = self.stream[self.stream_index]
 
-
-
-
 	def _whitespace(self):
 		while self.stream[self.stream_index].isspace():
 			self.stream_index += 1
 
 	def _number(self):
-		print(stream_index)
+		orig_index = self.stream_index
+
+		if self.stream[self.stream_index] not in non_zero_digits:
+			self.stream_index = orig_index
+			return None
+
+		number_str = ""
+		while self.stream[self.stream_index] in (digits + "."):
+			number_str += self.stream[self.stream_index]
+			self.stream_index += 1
+		
+		try:
+			number = float(number_str)
+			return Number(number)
+		except ValueError:
+			self.stream_index = orig_index
+			return None
+
 
 	def _factor(self):
-		pass
+		orig_index = self.stream_index
+
+		negateFlag = False
+		if self.stream[self.stream_index] == "-":
+			negateFlag = True
+			self.stream_index += 1
+
+		if self.stream[self.stream_index] == "(":
+			self.stream_index += 1
+			
+			node = self._expression()
+
+			if self.stream[self.stream_index] != ")":
+				self.stream_index = orig_index
+				return None
+			self.stream_index += 1
+		else:
+			node = self._number()
+		
+		if node is None:
+			self.stream_index = orig_index
+			return None
+		
+		return Factor(child,negateFlag)
+
 
 	def _term(self):
 		pass
